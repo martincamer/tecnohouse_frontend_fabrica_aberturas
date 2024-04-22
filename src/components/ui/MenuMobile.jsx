83019a483dfb8cd3b70ffea7d0eef95dd8f6db03
martincamer <martincamer011@gmail.com>
2024-04-22 09:03:39 -0300

    asd

--- src/components/ui/MenuMobile.jsx
+++ src/components/ui/MenuMobile.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export const MenuMobile = () => {
